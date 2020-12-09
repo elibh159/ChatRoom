704bd1c861beb3008b81dc5c7e5bf90c58b992a0
Eb Heravi <ebheravi159@gmail.com>
2020-12-09 15:46:55 +0330

    Add a create room form

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -14,21 +14,42 @@ const Home = () => {
   }
   const setAsTom = () => {
     const tom = {
-      name: 'Tli',
-      email: 'tli@email.com',
+      name: 'Tom',
+      email: 'tom@email.com',
       id: '456'
     }
     setUser(tom);
   }
   return (
     <div>
-      <h1>Home {JSON.stringify(user)}</h1>
-      <button onClick={setAsEli}>set as Eli</button>
-      <button onClick={setAsTom}>set as Tom</button>
-      <Link to={'/chat'}>
-        <button>go to chat</button>
-      </Link>
+
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">welcome {user ? user.name : ""}</span>
+              <form >
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input placeholder="Enter a room name" id="first_name" type="text" className="validate" />
+                    <label htmlfor="room">Room</label>
+                  </div>
+                </div>
+                <button className="btn">Create Room</button>
+              </form>
+            </div>
+            <div className="card-action">
+              <a onClick={setAsEli}>set as Eli</a>
+              <a onClick={setAsTom}>set as Tom</a>
+            </div>
+          </div>
+        </div>
+        <Link to={'/chat'}>
+          <button>go to chat</button>
+        </Link>
+      </div>
     </div>
+
   )
 }
 
