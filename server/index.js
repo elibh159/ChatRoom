b30aa2bd51d1607b0be528a8c6a23c24ea91d29f
Eb Heravi <ebheravi159@gmail.com>
2020-12-12 14:58:03 +0330

    Receive message at the server side

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const socketio = require('socket.io')
 const io = socketio(http);
-const { addUser } = require('./helper');
+const { addUser, getUser, removeUser } = require('./helper');
 const PORT = process.env.PORT || 5000
 
 io.on('connection', (socket) => {
@@ -22,6 +22,21 @@ io.on('connection', (socket) => {
     } else {
       console.log('join user', user)
     }
+  })
+  socket.on('sendMessage', (message, room_id, callback) => {
+    const user = getUser(socket.id);
+    const msgToStore = {
+      name: user.name,
+      user_id: user.user_id,
+      room_id,
+      text: message
+    }
+    console.log('message', msgToStore)
+    io.to(user.room_id).emit('message', msgToStore);
+    callback()
+  })
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
   })
 });
 
